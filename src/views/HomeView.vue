57613636194e65f50a82c0cc122fda2d9e0c9a27
Malek <omayri970@gmail.com>
2024-12-02 02:50:29 +0300

    example expose variable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const name = ref();
+
+name.value = 'Vue 3'
+window.nameRef = name; // expose variable to global scope whcih is a browser 
 </script>
 
 <template>
   <main>
-    <h1>This is a home page</h1>
+    <h1>{{ name }}</h1>
   </main>
 </template>
